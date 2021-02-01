3d43b7d31235089fe779389506c67706bfba6267
kemal <kemal@LAPTOP-J49F5T9Q>
2021-02-01 21:30:26 +0300

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new CarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                System.Console.WriteLine("Model:"+car.ModelYear+"\tBilgi:"+car.Description);
+            }
+        }
+    }
+}
